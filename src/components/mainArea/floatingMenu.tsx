68d320b584422b4da281a0b2fed44e9d1afe58f2
HJinS <windowed@naver.com>
2022-01-16 22:11:16 +0900

    2022-01-16 카테고리 디자인 정하는 중

--- src/components/mainArea/floatingMenu.tsx
+++ src/components/mainArea/floatingMenu.tsx
@@ -3,6 +3,10 @@ import { Grid, Box } from "@mui/material";
 import './mainArea.css'
 import Typography from '@mui/material/Typography';
 import { Button } from "@mui/material";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AccordionDetails from '@mui/material/AccordionDetails';
 
 function FloatingMenu() {
     const category=["intel cpu", "amd cpu",
@@ -12,15 +16,25 @@ function FloatingMenu() {
                     "liquid cpu cooler",
                     "air cpu cooler"]
     return (
-        <Box className={"FloatingMenuStyle"}>
-            {category.map(categoryItem => (
-                <Button className={"ButtonStyle"}>
-                    <Typography variant="subtitle1" color="text.secondary">
-                        {categoryItem}
-                    </Typography>
-                </Button>
-            ))}
-        </Box>
+        <Accordion>
+            <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header">
+                {"tmp"}
+                <AccordionDetails>
+                    <Box className={"FloatingMenuStyle"}>
+                        {category.map(categoryItem => (
+                            <Button className="ButtonStyle" variant="contained" color="inherit">
+                                <Typography variant="body2" color="text.secondary">
+                                    {categoryItem}
+                                </Typography>
+                            </Button>
+                        ))}
+                    </Box>
+                </AccordionDetails>
+            </AccordionSummary>
+        </Accordion>
     );
   }
   
