80406c832466a87f1be74c4109c71cb768f4c127
HJinS <windowed@naver.com>
2022-03-25 22:31:49 +0900

    2022-03-25 webpack 테스트

--- src/index.js
+++ src/index.js
@@ -6,19 +6,11 @@ import axios from 'axios';
 import rootReducer from './modules/__reducers/reducer_setting';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import _ from 'lodash';
+
 
 const store = createStore(rootReducer);
 
 axios.defaults.withCredentials = true;
-
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
 
 ReactDOM.render(
   <Provider store={store}>
@@ -26,4 +18,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-document.body.appendChild(component());
